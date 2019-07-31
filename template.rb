d507d6164e245be0ac6eaff192c6b76f82d403ae
arinthros <arinthros@gmail.com>
2019-07-31 13:50:46 -0400

    Deleted git file path code

--- template.rb
+++ template.rb
@@ -1,30 +1,6 @@
 require "fileutils"
 require "shellwords"
 
-# Copied from: https://github.com/mattbrictson/rails-template
-# Add this template directory to source_paths so that Thor actions like
-# copy_file and template resolve against our source files. If this file was
-# invoked remotely via HTTP, that means the files are not present locally.
-# In that case, use `git clone` to download them to a local temporary dir.
-def add_template_repository_to_source_path
-  if __FILE__ =~ %r{\Ahttps?://}
-    require "tmpdir"
-    source_paths.unshift(tempdir = Dir.mktmpdir("template-"))
-    at_exit { FileUtils.remove_entry(tempdir) }
-    git clone: [
-      "--quiet",
-      "https://github.com/arinthros/template.git",
-      tempdir
-    ].map(&:shellescape).join(" ")
-
-    if (branch = __FILE__[%r{template/(.+)/template.rb}, 1])
-      Dir.chdir(tempdir) { git checkout: branch }
-    end
-  else
-    source_paths.unshift(File.dirname(__FILE__))
-  end
-end
-
 def rails_version
   @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
 end
