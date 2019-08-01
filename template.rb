ea3bcde852720929c26e5f56571ef7857c6d6913
arinthros <arinthros@gmail.com>
2019-08-01 15:35:45 -0400

    Remove support for Rails < 6.0.0.rc2

--- template.rb
+++ template.rb
@@ -29,14 +29,6 @@ def rails_version
   @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
 end
 
-def rails_5?
-  Gem::Requirement.new(">= 5.2.0", "< 6.0.0.beta1").satisfied_by? rails_version
-end
-
-def rails_6?
-  Gem::Requirement.new(">= 6.0.0.beta1", "< 7").satisfied_by? rails_version
-end
-
 def add_gems
   gem 'administrate', github: "thoughtbot/administrate"
   gem 'bootstrap', '~> 4.3', '>= 4.3.1'
@@ -47,20 +39,11 @@ def add_gems
   gem 'friendly_id', '~> 5.2', '>= 5.2.5'
   gem 'name_of_person', '~> 1.1'
   gem 'sidekiq', '~> 5.2', '>= 5.2.7'
-
-  if rails_5?
-    gsub_file "Gemfile", /gem 'sqlite3'/, "gem 'sqlite3', '~> 1.3.0'"
-    gem 'webpacker', '~> 4.0.1'
-  end
 end
 
 def set_application_name
   # Add Application Name to Config
-  if rails_5?
-    environment "config.application_name = Rails.application.class.parent_name"
-  else
-    environment "config.application_name = Rails.application.class.module_parent_name"
-  end
+  environment "config.application_name = Rails.application.class.module_parent_name"
 
   # Announce the person where he can change the application name in the future.
   puts "You can change application name inside: ./config/application.rb"
@@ -90,7 +73,7 @@ def add_people
     gsub_file migration, /:admin/, ":admin, default: false"
   end
 
-  if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
+  if Gem::Requirement.new("> 6.0.0.rc2").satisfied_by? rails_version
     gsub_file "config/initializers/devise.rb",
       /  # config.secret_key = .+/,
       "  config.secret_key = Rails.application.credentials.secret_key_base"
@@ -100,21 +83,8 @@ def add_people
   inject_into_file("app/models/person.rb", "masqueradable, :", after: "devise :")
 end
 
-def add_webpack
-  # Rails 6+ comes with webpacker by default, so we can skip this step
-  return if rails_6?
-
-  # Our application layout already includes the javascript_pack_tag,
-  # so we don't need to inject it
-  rails_command 'webpacker:install'
-end
-
 def add_javascript
   run "yarn add expose-loader jquery popper.js bootstrap data-confirm-modal local-time"
-
-  if rails_5?
-    run "yarn add turbolinks @rails/actioncable@pre @rails/actiontext@pre @rails/activestorage@pre @rails/ujs@pre"
-  end
 
   content = <<-JS
 const webpack = require('webpack')
@@ -215,7 +185,6 @@ after_bundle do
   set_application_name
   stop_spring
   add_people
-  add_webpack
   add_javascript
   add_sidekiq
   add_friendly_id
